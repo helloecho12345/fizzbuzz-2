1395c2c9ce5cd52647b9b314c8d36309ffbbac1f
mikitaro-27 <matt.diak@gmail.com>
2021-11-29 17:24:06 +0000

    adjust fizzbuzz.spec.rb to check for any number divisble by 3 to return fizz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,28 @@
 require_relative '../lib/fizzbuzz'
 
+# Create a test for fizz
+
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 end
+
+# Add more cases for 9, 27
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 9' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 9' do
+    expect(fizzbuzz(27)).to eq 'fizz'
+  end
+end
+
+# Add more cases to chec for 25, 100
 
 # Create a test for buzz
 
@@ -14,10 +32,17 @@ describe 'fizzbuzz' do
   end
 end
 
+# Add more cases to check for 30, 60 
+
 # Create a test for fizzbuzz
 
 describe 'fizzbuzz' do
   it 'returns "fizzbuzz" if number passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+
+# Create a test for number if is it not divisible by 3 or 5
+
+# Add cases for 7, 41
+
 end
