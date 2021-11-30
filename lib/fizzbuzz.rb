f1475c97bf18a058959d498c5f2079068a90165c
mikitaro-27 <matt.diak@gmail.com>
2021-11-30 12:31:31 +0000

    refactor fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,17 +1,21 @@
 def fizzbuzz(number)
-  # when number is 3 return fizz
-  if number % 3 == 0 && number % 5 != 0 
-    "fizz"
-  # when number is divisible 5, return "buzz"
-  elsif number % 5 == 0 && number % 3 != 0
-    "buzz"
-  # when number is divisible by 5 and 3, return "fizzbuzz"
-  elsif number % 3 == 0 && number % 5 == 0
+
+  # when number is divisible by 5 and 3; return "fizzbuzz"
+  
+  if number % 3 == 0 && number % 5 == 0
     "fizzbuzz"
+  
+  # when number is divisible by 3; return fizz
+  elsif number % 3 == 0
+    "fizz"
+  
+  # when number is divisible 5; return buzz
+  elsif number % 5 == 0
+    "buzz"
+  
   # For number not divisible by 3 or 5; return number
   else
     number
   end
-  # when number is 5 return buzz
 end
 
