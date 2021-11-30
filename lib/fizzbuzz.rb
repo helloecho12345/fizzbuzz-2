fb5fae752af2b9d3c13a783a9cf219d6089e6bc1
Esther <helloecho12345@gmail.com>
2021-11-30 12:12:24 +0000

    add conditions for number divisible by 3 and 5 to return fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -5,7 +5,8 @@ def fizzbuzz(number)
   # when number is divisible 5, return "buzz"
   elsif number % 5 == 0 && number % 3 != 0
     "buzz"
-  elsif number == 15
+  # when number is divisible by 5 and 3, return "fizzbuzz"
+  elsif number % 3 == 0 && number % 5 == 0
     "fizzbuzz"
   end
   # when number is 5 return buzz
